cf6dc303eab483bbac22c6ca41d70c8e75ef0dc4
Jonas <publik.froeller.jonas@gmx.at>
2024-02-21 17:38:43 +0100

    fix(github/probot): initial probot

--- src/v1/adapters/github/probot/src/index.ts
+++ src/v1/adapters/github/probot/src/index.ts
@@ -1,11 +1,13 @@
 import { Probot } from "probot";
 
+// https://probot.github.io/docs/webhooks
+// https://github.com/octokit/webhooks.js/#webhook-events
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.onAny(async (context) => {
+    // https://probot.github.io/docs/github-api
+    app.log.info({ event: context.name, payload: context.payload });
+
+    // inform elysiajs microservice that a change has been made to the repository
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
