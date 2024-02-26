6d49494a753d3028daee79a5c446e007287cbf32
Jonas <publik.froeller.jonas@gmx.at>
2024-02-26 12:00:58 +0100

    chore(github/probot): moved to new repository: `propromo.probot`

--- src/v1/adapters/github/probot/src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { createNodeMiddleware, createProbot } from "probot";
-import app from "./index";
-
-module.exports = createNodeMiddleware(app, {
-    probot: createProbot(),
-    webhooksPath: "/v1/github/webhooks",
-});
