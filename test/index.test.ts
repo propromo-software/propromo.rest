3676ab5af9ddd91450b9f99dd2f0a30f1732e168
Jonas <publik.froeller.jonas@gmx.at>
2024-04-02 14:42:23 +0200

    test(`/api`): testing forwarding and presence of documentation content

--- test/index.test.ts
+++ test/index.test.ts
@@ -26,6 +26,39 @@ describe('Propromo Rest-Microservice Tests (ElysiaJS Tests)', () => {
         }
     })
 
+    it('/api redirects to a openapi documentation page, that is being rendered correctly', async () => {
+        const response = await app
+            .handle(new Request('http://localhost:3000/api', {
+                redirect: "follow" // redirects to the latest (/<latest_version>/api)
+            }))
+            .then(async (res) => {
+                const response = await app
+                    .handle(new Request(`http://localhost:3000/${res.headers.get('location')}`))
+                    .then((res) => res.text());
+            
+                return response;
+            })
+        
+        expect(response).toBeString();
+
+        // /api doesn't return valid html, (error in the library used, not in the code of the microservice) :|
+        /* try {
+            const options = {
+                // url: 'http://localhost:3000/', // depends on axios and would require to manually start the development server...
+                // isLocal: true,
+                data: response
+            }
+
+            const result = await validator(options); // returns "The document validates according to the specified schema(s)." :skull:
+            console.log(result);
+
+            expect(result).toBeString();
+        } catch (error) {
+            console.error(error);
+            expect(true).toBe(false);
+        } */
+    })
+
     it('openapi specification is returned', async () => {
         const response = await app
             .handle(new Request('http://localhost:3000/api/json', {
